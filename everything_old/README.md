3f095f391df32c83e0fb0582a9055ecc5660d2e2
Ben Yang <yang.benjamin1998@gmail.com>
2022-01-18 00:46:18 -0500

    Updates

--- everything_old/README.md
+++
@@ -1,3 +0,0 @@
-## Personal Website
-
-Past website looked too boring and like a resume; changed it so title card looked cool, will probably add more pages with time.
---
+++ precache-manifest.a186118d7f14900f568763081b814f48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d83327a355245d432f295f32cb34fb",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8d75227283e01c5a1109",
+    "url": "/home/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d75227283e01c5a1109",
+    "url": "/home/static/js/main.22f11a8c.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "6175d14e16a9e34fe2d8b50c2fb67930",
+    "url": "/home/static/media/benyang.6175d14e.png"
+  },
+  {
+    "revision": "6c3eb3623da691a570acc19883f214f0",
+    "url": "/home/static/media/resume.6c3eb362.pdf"
+  }
+]);
